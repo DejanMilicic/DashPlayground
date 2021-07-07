01ecc0bd08863b58d27a8837db1dbb6ee796a3ea
Dejan Milicic <dejan.milicic@gmail.com>
2021-07-07 16:58:32 +0200

    initial commit

---
+++ DashStocks/giraffe.fs
@@ -0,0 +1,81 @@
+﻿module giraffe
+
+
+open System
+open System.IO
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Cors.Infrastructure
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+
+open Dash.NET
+open dash_stock_ticker
+
+// ---------------------------------
+// Error handler
+// ---------------------------------
+
+let errorHandler (ex: Exception) (logger: ILogger) =
+    logger.LogError(ex, "An unhandled exception has occurred while executing the request.")
+
+    clearResponse
+    >=> setStatusCode 500
+    >=> text ex.Message
+
+// ---------------------------------
+// Config and Main
+// ---------------------------------
+
+let configureCors (builder: CorsPolicyBuilder) =
+    builder
+        .WithOrigins("http://localhost:8080")
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+    |> ignore
+
+let configureApp (app: IApplicationBuilder) =
+    let env =
+        app.ApplicationServices.GetService<IWebHostEnvironment>()
+
+    (match env.EnvironmentName with
+     | "Development" -> app.UseDeveloperExceptionPage()
+     | _ -> app.UseGiraffeErrorHandler(errorHandler))
+        .UseHttpsRedirection()
+        .UseCors(configureCors)
+        .UseStaticFiles()
+        .UseGiraffe(DashApp.toHttpHandler myDashApp)
+
+let configureServices (services: IServiceCollection) =
+    services.AddCors() |> ignore
+    services.AddGiraffe() |> ignore
+
+let configureLogging (builder: ILoggingBuilder) =
+    builder
+        .AddFilter(fun l -> l.Equals LogLevel.Debug)
+        .AddConsole()
+        .AddDebug()
+    |> ignore
+
+[<EntryPoint>]
+let main args =
+    let contentRoot = Directory.GetCurrentDirectory()
+    let webRoot = Path.Combine(contentRoot, "WebRoot")
+
+    Host
+        .CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(fun webHostBuilder ->
+            webHostBuilder
+                .UseContentRoot(contentRoot)
+                .UseWebRoot(webRoot)
+                .UseUrls("http://localhost:5000")
+                .Configure(Action<IApplicationBuilder> configureApp)
+                .ConfigureServices(configureServices)
+                .ConfigureLogging(configureLogging)
+            |> ignore)
+        .Build()
+        .Run()
+
+    0
